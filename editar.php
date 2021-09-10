2d08ababf93af7fb8ee673c28dd9b56ef54f3441
Juan Camilo Valencia <76007597+Jc2001valencia@users.noreply.github.com>
2021-09-09 21:53:19 -0500

    Add files via upload

---
+++ editar.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="sweetalert2.min.css">
+    <title>Document</title>
+ <style>
+body{
+    background: linear-gradient(70deg, black, white);}
+     #container{
+  
+  margin: 1rem;
+  padding: 1rem;
+  border: 2px solid #ccc;
+  align-items: center;
+  text-align: center;
+
+     }
+     
+     
+     </style>
+
+</head>
+<body>
+
+
+<?php
+include'conexion.php';
+
+$id=$_GET['id'];
+$sql="SELECT * FROM `persona` WHERE id='$id'";
+
+$resultado=mysqli_query($con,$sql);
+while ($fila = mysqli_fetch_assoc($resultado)) {
+    
+?>
+<div id="container" class="form-group col-md-4">
+
+<form>
+<label for=""><i class="fa fa-id-card"></i>ID</label>
+<input class="form-control"type="text" name="txtid" value="<?php echo $fila['id']?>" readonly="readonly"><br>
+<label for=""><i class="fa fa-user"></i>NOMBRES</label>
+<input class="form-control"type="text" name="txtuser"  value="<?php echo $fila['nombre']?>" ><br>
+<label for=""><i class="fa fa-envelope"></i>CORREO</label>
+<input class="form-control" type="text" name="txtcorreo" value="<?php echo $fila['correo']?>" required  pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}"><br>
+<label for=""><i class="fa fa-phone"></i>TELEFONO</label>
+<input  class="form-control" type="text" name="txttel" value="<?php echo $fila['telefono']?>"><br>
+<input class="btn btn-primary" type="submit" name="" value="Actualizar">
+<a class="btn btn-outline-warning" href="index.php">regresar</a>
+
+</form>
+
+</div>
+<?php
+}
+?>
+
+
+<?php
+
+include'conexion.php';
+$idp=$_GET['txtid'];
+$user=$_GET['txtuser'];
+$correo=$_GET['txtcorreo'];
+$tel=$_GET['txttel'];
+if ($user!=null||$correo!=null||$tel=null) {
+
+    $sql2="update persona set nombre ='".$user."',correo='".$correo."',telefono='".$tel."' WHERE id='".$idp."'";
+    mysqli_query($con,$sql2);
+
+    echo'
+    
+    <script>
+    
+    alert("usuario almacenado exitosamente")
+
+    window.location="index.php";
+    </script>
+    ';
+
+    if ($user=1) {
+
+        
+        echo'
+        <script>
+        
+        alert("usuario no fue almacenado exitosamente")
+    
+       
+        </script>
+        ';
+    
+    }
+
+}
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</html>
+
---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<style>
+body{
+    background: linear-gradient(70deg, black, white);
+}
+
+#tabla{
+    margin: 100px;
+    position: relative; 
+    width:55%
+
+}
+#agregar{
+
+  width: 200px;
+  margin: 50px;
+}
+
+
+
+
+</style>
+<body>
+
+
+
+<?php
+
+
+include'conexion.php';
+
+$query = "SELECT * FROM persona";
+$result = mysqli_query($con, $query);
+?>
+
+
+
+<div>
+
+<a   id="agregar"   href="agregar.php"  class="btn btn-primary btn-block"><i class="fa fa-plus-square"></i>Nuevo</a>
+ <table id="tabla" class="table table-striped table-bordered table-hover table-dark" >
+
+<thead>
+
+<tr>
+    <th>ID</th>
+    <th>NOMBRES</th>
+    <th>CORREO</th>
+    <th>TELEFONO</th>
+    <th>ACCIONES</th>
+    
+</tr>
+
+</thead>
+<tbody>
+<?php
+
+while($fila = mysqli_fetch_assoc($result)) { 
+   
+
+
+?>
+<tr>
+    <th><?php echo $fila['id']?></th>
+    <th><?php echo $fila['nombre']?></th>
+    <th><?php echo $fila['correo']?></th>
+    <th><?php echo $fila['telefono']?></th>
+    <th><a href="editar.php?id=<?php echo $fila['id'] ?>" class="btn btn-success"><i class="fa fa-edit"></i>Editar</a> <a  href="eliminar.php?id=<?php echo $fila['id'] ?>" class="btn btn-danger"><i class="fa fa-trash"></i>Eliminar</a></th>
+    
+</tr>
+
+<?php
+}
+?>
+
+
+
+
+</tbody>
+
+
+ </table>
+ 
+ 
+
+
+
+
+
+</div>
+
+
+    
+</body>
+</html>
